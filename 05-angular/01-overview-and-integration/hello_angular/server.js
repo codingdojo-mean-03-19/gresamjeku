6f3b3e0701180a75ff9247bb580d03d4fd2c45bb
MjekuGresa <gresa.mjeku@outlook.com>
2019-04-13 13:39:57 +0200

    First Angular App

---
+++ 05-angular/01-overview-and-integration/hello_angular/server.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var session = require('express-session');
+var app = express();
+
+app.use(session({
+  secret: 'keyboardkitteh',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 }
+}))
+
+app.set('views', __dirname + '/views'); 
+app.set('view engine', 'ejs');
+app.use(express.static( __dirname + '/public/dist/public' ));
+
+app.get("/", function (request, response){
+  if(request.session.count){ request.session.count++;}
+  else { request.session.count = 1;}
+  response.render('index', {count: request.session.count});
+});
+
+app.get('/add_2_counts', function(request, response){
+  request.session.count++;
+  response.redirect('/');
+});
+
+app.get('/reset_counts', function(request,response){
+  request.session.destroy();
+  response.redirect('/');
+});
+
+app.listen(8000, function() {
+  console.log("listening on port 8000");
+})
